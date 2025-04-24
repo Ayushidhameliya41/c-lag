dc7d7e9077974162ef49c9da3c6822006c641219
Ayushidhameliya41 <ayushidhameliya51@gmail.com>
2025-04-24 13:30:26 +0530

    add new folder

---
+++ UNIT-3/Q-2.c
@@ -0,0 +1,13 @@
+//( x - y )( x - y )
+#include<stdio.h>
+int main()
+{
+    int x,y,ans;
+    printf("Enter x :- ");
+    scanf("%d",&x);
+    printf("Enter y :- ");
+    scanf("%d",&y);
+    ans=( x - y )*( x - y );
+    printf("ans = %d",ans);
+    return 0;
+}
